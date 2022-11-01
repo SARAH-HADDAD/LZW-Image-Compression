0754eac900880d86279ea41633e6b44f8a10f6af
Haddad Sarah <haddads686@gmail.com>
2022-11-01 20:53:08 +0100

    compress and decompress an image in black and white

--- lzw.py
+++ lzw.py
@@ -1,24 +1,33 @@
 import argparse
 import os
 import pickle
+import cv2
+from PIL import Image
+import numpy as np
+
 
 DICTIONARY_SIZE = 256
+#Transformer ces deux codes pour leur permettre de compresser et décompresser une
+#image (noir et blanc).
 
-def compress(input):
+def compress(input,bits):
     #print(type(input)) le type de input est byte
     global DICTIONARY_SIZE
+    DICTIONARY_SIZE=2**bits
     dictionary = {}
     result = []
     temp = ""
 
     for i in range(0, DICTIONARY_SIZE):
-        dictionary[str(chr(i))] = i
-
+        #str(i).zfill(3)
+        dictionary[str(i)] = i
+    
+    #print(dictionary)
     #Lecture du caractère suivant:
     for c in input:# le type de c est int
         #temp howa w
         #temp2 howa k le caractère suivant 
-        temp2 = temp+str(chr(c))
+        temp2 = temp+str(c)
         if temp2 in dictionary.keys(): #if wk fi Dictionary
             temp = temp2 #w = wk
         else:
@@ -27,25 +36,27 @@ def compress(input):
             dictionary[temp2] = DICTIONARY_SIZE #Dictionary <- wk
             DICTIONARY_SIZE+=1
             #Mise à jour de la sous chaine temporaire (w):
-            temp = ""+str(chr(c)) #w=k
+            temp = ""+str(c) #w=k
 
     if temp != "":
         result.append(dictionary[temp])  
-        
+    print(result)    
     return result
 
-def decompress(input):
+def decompress(input,bits):
     #print(type(input)) le type de input est une liste
     global DICTIONARY_SIZE
+    DICTIONARY_SIZE=2**bits
     dictionary = {}
     result = []
 
+    #padding pour les valeurs avec 2 chifres ou 1
     for i in range(0, DICTIONARY_SIZE):
-        dictionary[i] = str(chr(i))
+        dictionary[i] = str(i)
 
     # Read k: 
     # Lecture du code k suivant  
-    previous = chr(input[0])
+    previous = str(input[0])
     input = input[1:] 
     result.append(previous) # Output Dict[k]
     
@@ -76,28 +87,54 @@ def decompress(input):
         previous = aux
     return result
 
-parser = argparse.ArgumentParser(description = 'Text compressor and decompressor.')
-parser.add_argument('action', choices={"compress", "decompress"}, help="Define action to be performed.")
-parser.add_argument('-i', action = 'store', dest = 'input', required = True,
-                           help = 'Input file.')
-parser.add_argument('-o', action = 'store', dest = 'output', required = True,
-                           help = 'Output file.')
-arguments = parser.parse_args()
+def toImage(ListImage,width,height):
+    print("List Image:")
+    print(ListImage)
+    StrImage=''.join(map(str,ListImage))
+    print("Str Image:")
+    print(StrImage)
+    list = []
+    list[:0] = StrImage
+    print("Liste:")
+    print(list)
+    for i in range(len(list)): 
+        if list[i]=='1':
+            list[i]=255
+        else: list[i]=0   
+    # Convert the pixels into an array using numpy      
+    array=np.array(list, dtype=np.uint8)
+    array = np.reshape(array, (width,height))
+    # Use PIL to create an image from the new array of pixels
+    new_image = Image.fromarray(array)
+    new_image.save('decompressed.png')
 
-ABSOLUTE_PATH = os.getcwd()
 
-if arguments.action == 'compress':
-    input = open(ABSOLUTE_PATH+"//"+arguments.input, "rb").read()
-    output = open(ABSOLUTE_PATH+"//"+arguments.output, "wb")
 
-    compressedFile = compress(input)
-    pickle.dump(compressedFile, output)
-else:
-    input = pickle.load(open(ABSOLUTE_PATH+"//"+arguments.input, "rb"))
-    output = open(ABSOLUTE_PATH+"//"+arguments.output, "w")
-    
-    uncompressedFile = decompress(input)
-    for l in uncompressedFile:
-            output.write(l)
-    output.close()
+
+
+img_gray = cv2.imread('Image.png', cv2.IMREAD_GRAYSCALE)
+width,height = img_gray.shape
+# Applatir l'image
+string_image = img_gray .flatten().tolist()
+for i in range(len(string_image)): 
+    if string_image[i]==255:
+        string_image[i]=1
+print(string_image)
+print(type(string_image))
+bits=1
+result=compress(string_image,bits)
+ListImage=decompress(result,bits)
+toImage(ListImage,width,height)
+
+#The second argument is the threshold value which is used to classify the pixel values
+#The third argument is the maximum value which is assigned to pixel values exceeding the threshold
+#_, img_bw = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY)
+#cv2.imwrite('newImage.png', img_bw)
+#height, width = img_bw.shape # 787x444 = 349428
+#print(type(img_bw))#numpy.ndarray
+#print(img_bw)
+#print(img_bw.tobytes())
+
+
+
            
